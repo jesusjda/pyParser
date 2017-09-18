453f25e7432b7346d4af9c0fc0d5ebad7b24975a
Jesus Domenech <jesusjda@gmail.com>
2017-09-18 16:58:18 +0200

    remove unused imports and apply code-style to tests

--- test/test_Parser.py
+++ test/test_Parser.py
@@ -1,11 +1,8 @@
-import difflib
 import unittest
-import filecmp
 import os as _os
 import sys as _sys
 import genericparser
 import lpi
-import pickle
 
 
 class TestKey(unittest.TestCase):
@@ -30,7 +27,6 @@ class TestKey(unittest.TestCase):
         print(o_list)
         self.assertItemsEqual(r_list, o_list)
 
-
     def test_parser_mlc(self):
         p = _os.path.dirname(_os.path.abspath(__file__))
         testfile = _os.path.join(p, "../genericparser/examples/example.mlc")
@@ -50,4 +46,3 @@ class TestKey(unittest.TestCase):
         print("ORIGINAL")
         print(o_list)
         self.assertItemsEqual(r_list, o_list)
-
