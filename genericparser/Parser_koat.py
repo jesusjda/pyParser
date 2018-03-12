7aaf1f9fca5106fdddebb49310148a32c936053e
jesusjda <jesusjda@gmail.com>
2018-03-12 19:00:07 +0100

    Koat parser

--- genericparser/Parser_koat.py
+++ genericparser/Parser_koat.py
@@ -1,12 +1,12 @@
 from genericparser.Constraint_parser import ConstraintTreeTransformer
 from genericparser import ParserInterface
 from genericparser.Cfg import Cfg
+from .expressions import expterm
 
-
-class Parser_mlc(ParserInterface):
+class Parser_koat(ParserInterface):
     
     def parse(self, filepath, debug=False):
-        """Parse .mlc file
+        """Parse .koat file
 
         :param filepath: Full path to file to be parsed.
         :type filepath: str
@@ -20,12 +20,12 @@ class Parser_mlc(ParserInterface):
     
     def parse_string(self, cad, _=None, debug=False):
         import os
-        grammarfile = os.path.join(os.path.dirname(__file__),"mlc.g")
+        grammarfile = os.path.join(os.path.dirname(__file__),"koat.g")
         with open(grammarfile, "r") as grammar:
             g = grammar.read()
         from lark.lark import Lark
         l = Lark(g)
-        return self.program2cfg(MlcTreeTransformer().transform(l.parse(cad)))
+        return self.program2cfg(KoatTreeTransformer().transform(l.parse(cad)))
 
     def program2cfg(self, program):
         G = Cfg()
@@ -41,44 +41,54 @@ class Parser_mlc(ParserInterface):
         return G
 
 
-class MlcTreeTransformer(ConstraintTreeTransformer):
+class KoatTreeTransformer(ConstraintTreeTransformer):
     
     name = lambda self, node: str(node[0])
-    transition = list
-    transitions = list
-    vars = pvars = list
+    entry = lambda self, node: node[0]
+    constraints = list
+    rules = variables = list
+    rule = node = list
+
     def start(self, node):
         program = {}
-        
-        g_vars = node[0]
-        if len(node) == 3:
-            pvars = node[1]
-            if len(g_vars) != len(pvars):
-                raise ValueError("Different number of variables and"
-                                 + " prime variables.")
-        else:
-            pvars = [v+"'" for v in g_vars]
-
-        program["global_vars"] = g_vars + pvars
+        program["goal"] = node[0]
+        entry = node[1]
+        g_vars = node[2]
+        N = len(g_vars)
+        transitions = node[3]
+        program["global_vars"] = g_vars + [v+"'" for v in g_vars]
+        g_vars = program["global_vars"]
         for i in range(len(program["global_vars"])-1):
             if program["global_vars"][i] in program["global_vars"][i+1:]:
                 raise ValueError("Multiple definition of variable: {}".format(program["global_vars"][i]))
-
-        program["transitions"] = node[-1]
 
         set_gvars = set(program["global_vars"])
         max_local_vars = 0
         trs = []
         count = 0
-        for t in program["transitions"]:
+        for t in transitions:
             tr = {}
-            tr["source"] = tr["target"] = "n"
-            tr["name"] = "t" + str(count)
-            count += 1
+            if len(t) == 3:
+                left, right, cons = t
+            else:
+                left, right = t
+                cons = []
+            tr["source"] = left.pop(0)
+            tr["target"] = right.pop(0)
+            tr["name"] = "t"+str(count)
+            count+=1
+            # add init constraints
+            for idx in range(len(left)):
+                if str(left[idx]) == g_vars[idx]:
+                    continue
+                cons.append(left[idx] == expterm(g_vars[idx]))
+            # add post constraints
+            for idx in range(len(right)):
+                cons.append(right[idx] == expterm(g_vars[N+idx]))
+            tr["constraints"] = cons
             linear = True
             l_vars = []
-            tr["constraints"] = t
-            for c in t:
+            for c in tr["constraints"]:
                 if not c.is_linear():
                     linear = False
                 ll_vars = [x for x in c.get_variables()
@@ -90,8 +100,10 @@ class MlcTreeTransformer(ConstraintTreeTransformer):
             tr["linear"] = linear
             tr["local_vars"] = l_vars
             trs.append(tr)
-        program.update(transitions=trs)
-        program["init_node"] = program["transitions"][0]["source"]
+        program["transitions"] =trs
+        if entry:
+            program["init_node"] = entry
+        else:
+            program["init_node"] = program["transitions"][0]["source"]
         program["max_local_vars"] = max_local_vars
-        print(program)
         return program
