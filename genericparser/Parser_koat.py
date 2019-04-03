0c58a08fcf29a5c542cdcd77244c75b49bd681f6
Jesus Domenech <jesusjda@gmail.com>
2019-04-03 15:06:52 +0200

    koat accepts Com_N

--- genericparser/Parser_koat.py
+++ genericparser/Parser_koat.py
@@ -32,107 +32,105 @@ class Parser_koat(ParserInterface):
 class KoatTreeTransformer(ConstraintTreeTransformer):
 
     name = lambda self, node: str(node[0])
+    number = lambda self, node: int(node[0])
     entry = lambda self, node: node[0]
     constraints = list
-    rules = variables = list
-    rule = node = list
+    variables = list
     noentry = lambda self, __: None
 
+    trs_count = 0
+    variable_list = None
+    pvars = None
+    node_data = {}
+    max_local_vars = 0
+
+    def rules(self, node):
+        trs = []
+        for n in node:
+            trs += n
+        return trs
+
+    def node(self, tnode):
+        return tnode[0], tnode[1:]
+
+    def rule(self, node):
+        if len(node) == 3:
+            left, right, cons = node
+        elif len(node) == 2:
+            left, right = node
+            cons = []
+        else:
+            raise ValueError("too many tokens for rule")
+        src_name, src_vars = left
+        if src_name not in self.node_data:
+            self.node_data[src_name] = {"Com": []}
+        if len(right) > 1:
+            self.node_data[src_name]["Com"].append(len(right))
+        if self.variable_list is None:
+            self.variable_list = [str(v) for v in src_vars]
+            self.pvars = [v + "'" for v in self.variable_list]
+        else:
+            if len(self.variable_list) != len(src_vars):
+                raise ValueError("variables are not uniform.")
+            for v1, v2 in zip(self.variable_list, src_vars):
+                if v1 != str(v2):
+                    raise ValueError("variables are not uniform.")
+        base_lvars = []
+        base_linear = True
+        for c in cons:
+            if not c.is_linear():
+                base_linear = False
+            for v in c.get_variables():
+                if v not in self.variable_list and v not in base_lvars:
+                    base_lvars.append(v)
+        trs = []
+        for trg, trg_exp in right:
+            if len(trg_exp) != len(self.variable_list):
+                raise ValueError("node arguments doesn't match at transition: {} -> {}".format(src_name, trg))
+            if trg not in self.node_data:
+                self.node_data[trg] = {"Com": []}
+            linear = base_linear
+            lvars = list(base_lvars)
+            final_cons = list(cons)
+            for exp, pv in zip(trg_exp, self.pvars):
+                if not exp.is_linear():
+                    linear = False
+                for v in exp.get_variables():
+                    if v not in self.variable_list and v not in lvars:
+                        lvars.append(v)
+                final_cons.append(exp == Expression(pv))
+            tr = {}
+            tr["source"] = src_name
+            tr["target"] = trg
+            tr["name"] = "t" + str(self.trs_count)
+            tr[constants.transition.constraints] = final_cons
+            tr[constants.transition.localvariables] = lvars
+            tr[constants.transition.islinear] = linear
+            if len(lvars) > self.max_local_vars:
+                self.max_local_vars = len(lvars)
+            self.trs_count += 1
+            trs.append(tr)
+        return trs
+
+    def right_hand(self, node):
+        if len(node) > 1 and len(node) == node[0] + 1:
+            return node[1:]
+        elif len(node) == 1:
+            return node
+        else:
+            raise ValueError("The number of target nodes are not correct")
+
     def start(self, node):
-
-        def check_node(node, data, nodes):
-            new_num, vrs = data
-            if node in nodes:
-                num, vs = nodes[node]
-                if new_num == num:
-                    if vs == []:
-                        nodes[node] = (num, vrs)
-                    elif vrs != []:
-                        for i in range(num):
-                            if str(vs[i]) != str(vrs[i]):
-                                raise ValueError("Node {} with different args name.".format(node))
-                    return nodes
-                else:
-                    raise ValueError("Node {} with different num of args. {} != {}".format(node, num, new_num))
-            else:
-                nodes[node] = (new_num, vrs)
-                return nodes
-
         program = {}
         program["goal"] = node[0]
         entry = node[1]
-        transitions = node[3]
+        program["transitions"] = node[3]
         # defined_vars = [str(v) for v in node[2]]
-        max_local_vars = 0
-        tmp_trs = []
-        trs = []
-        count = 0
-        nodes = {}
-        if len(transitions) > 0:
-            N = len(transitions[0][0][1:])
-            for t in transitions:
-                if len(t[0]) != N + 1 or len(t[1]) != N + 1:
-                    raise ValueError("There are nodes with different number of variables")
-            vs = [str(v) for v in transitions[0][0][1:]]  # defined_vars[:N]
-            pvs = [v + "'" for v in vs]
-            program[constants.variables] = vs + pvs
-            g_vars = program[constants.variables]
-            for i in range(len(program[constants.variables]) - 1):
-                if program[constants.variables][i] in program[constants.variables][i + 1:]:
-                    raise ValueError("Multiple definition of variable: {}".format(program[constants.variables][i]))
-
-            set_gvars = set(program[constants.variables])
-
-            for t in transitions:
-                tr = {}
-                if len(t) == 3:
-                    left, right, cons = t
-                else:
-                    left, right = t
-                    cons = []
-                tr["source"] = left.pop(0)
-                nodes = check_node(tr["source"], (len(left), left), nodes)
-                trg = right.pop(0)
-                tr["target"] = trg
-                nodes = check_node(tr["target"], (len(right), []), nodes)
-                tr["name"] = "t" + str(count)
-                tr["tmp_cons"] = cons
-                tr["tmp_right"] = right
-                count += 1
-                tmp_trs.append(tr)
-
-            for tr in tmp_trs:
-                cons = tr["tmp_cons"]
-                del tr["tmp_cons"]
-                right = tr["tmp_right"]
-                del tr["tmp_right"]
-                target = tr["target"]
-                trg_num, trg_args = nodes[target]
-                # add post constraints
-                if len(trg_args) == trg_num:
-                    for idx in range(trg_num):
-                        vindex = g_vars.index(str(trg_args[idx]))
-                        cons.append(right[idx] == Expression(g_vars[N + vindex]))
-                tr[constants.transition.constraints] = cons
-                linear = True
-                l_vars = []
-                for c in tr[constants.transition.constraints]:
-                    if not c.is_linear():
-                        linear = False
-                    ll_vars = [x for x in c.get_variables()
-                               if x not in set_gvars]
-                    l_vars.extend(x for x in ll_vars
-                                  if x not in l_vars)
-                if len(l_vars) > max_local_vars:
-                    max_local_vars = len(l_vars)
-                tr[constants.transition.islinear] = linear
-                tr[constants.transition.localvariables] = l_vars
-                trs.append(tr)
-
-        program["transitions"] = trs
+        program[constants.variables] = self.variable_list + self.pvars
+        program["nodes"] = self.node_data
         if entry:
             program[constants.initnode] = entry
         else:
             program[constants.initnode] = program["transitions"][0]["source"]
-        program["max_local_vars"] = max_local_vars
+        program["max_local_vars"] = self.max_local_vars
         return program
